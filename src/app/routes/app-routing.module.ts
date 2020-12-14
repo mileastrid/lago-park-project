15939304e26e2e790fbd950eebb5a01e4fc59a6a
mileastrid <mileastrid2000@gmail.com>
2020-12-13 23:39:10 -0700

    Feat: Add Routing

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { LoginComponent } from '../pages/login/login.component';
+import { MenuComponent } from '../pages/menu/menu.component';
+import { RecreacionComponent } from '../pages/recreacion/recreacion.component';
+import { SignupComponent } from '../pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+  { path: 'recreacion', component: RecreacionComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
