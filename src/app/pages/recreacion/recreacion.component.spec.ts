cd3be618d74a4e3ce991bc83b2089c1aa2437030
mileastrid <mileastrid2000@gmail.com>
2020-12-13 23:39:52 -0700

    Feat: Add Recreacion Component

---
+++ src/app/pages/recreacion/recreacion.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecreacionComponent } from './recreacion.component';
+
+describe('RecreacionComponent', () => {
+  let component: RecreacionComponent;
+  let fixture: ComponentFixture<RecreacionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecreacionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecreacionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/recreacion/recreacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recreacion',
+  templateUrl: './recreacion.component.html',
+  styleUrls: ['./recreacion.component.scss']
+})
+export class RecreacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
