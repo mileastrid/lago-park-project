7a32709ce0d3a3a3b0eabf88c7426e9b8efed008
mileastrid <mileastrid2000@gmail.com>
2020-12-13 23:43:47 -0700

    Feat: Add Componets to Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ContainerComponent } from './components/container/container.component';
+import { RecreacionComponent } from './pages/recreacion/recreacion.component';
+import { TagMenuComponent } from './components/tag-menu/tag-menu.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent],
+  declarations: [AppComponent, NavigationComponent, HomeComponent, MenuComponent, LoginComponent, SignupComponent, ContainerComponent, RecreacionComponent, TagMenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
